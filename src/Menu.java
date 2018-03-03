ff506c9a481366dac6e50667216062294ed56269
Cre3d <isoreh@gmail.com>
2018-03-03 16:09:09 +0100

    Menu
    Rozwinięcie programu, dodanie menu.

---
+++ src/Menu.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Menu {
+    /**
+     *
+     * @param integral
+     * @return
+     */
+
+    public Integral menu(Integral integral)
+    {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Kalkulator całkowania");
+        System.out.println("_____________________");
+
+        integral.setXp(2);
+        integral.setXk(5);
+        integral.setN(3);
+
+        return integral;
+
+    }
+
+}
